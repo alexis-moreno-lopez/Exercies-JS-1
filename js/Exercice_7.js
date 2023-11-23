73236876612f8d5ebdfd5c26289bcec8b6200432
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2023-11-23 19:01:42 +0100

    exo_7

---
+++ js/Exercice_7.js
@@ -0,0 +1,8 @@
+let pointureChaussure = prompt("entrez votre pointure de chaussure ");
+let anneeNaissance = prompt("entrez votre année de naissance ");
+
+function multiplication(nombre1,nombre2){
+
+    console.log(nombre1 * nombre2)
+}
+multiplication(43 ,2 )
